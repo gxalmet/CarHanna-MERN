988550cfacc5e0aa94f6c47e40b97cb8079d9165
gxalmet <gxalmet@gmail.com>
2021-03-01 11:10:52 +0100

    sw

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import * as serviceWorkerRegistration from './public/serviceWorkerRegistration';
+import * as serviceWorkerRegistration from '../public/serviceWorkerRegistration';
 
 ReactDOM.render(
   <Provider store={store}>
